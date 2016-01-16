d2c0c6ddb64866525fd1976b8089189e158b2d2c
Szymen <szymmaslowski@gmail.com>
2016-01-16 03:17:38 +0100

    Webcrawler: - popakowane do funkcji. nadal działa

--- main.py
+++ main.py
@@ -1,151 +1,145 @@
 import sys, urllib.robotparser, urllib.request, re
 
-global can_browse
-global in_host
-global robots_broken
-global out_host
-global rp
-global base_url
-
-can_browse = True
-robots_broken = True
-out_host = {}
-in_host = {}
-base_url = ''
-
-
-def look_for_links(page_in_string):
-
-    for ref in re.findall('(src|href)="(\S+)"', page_in_string):
-        ref = ref[1]
-
-        if ref.startswith('/'): #it`s link to some sub-site
-            ref = "{0}{1}".format(link , ref)
-            if not ref.endswith('.html') and ref.split('/').__len__() > 3 and '.' in  ref.split('/')[ref.split('/').__len__()-1] : #it`s probably css or jpg or whateva; ends up with .../<random>.<sth>
-                continue #ignore it. it`s crap.
-            #print ('>>>>>',ref)
-            if robots_broken:
-                #print("Can fetch! << robots_broken")
-                in_host[ref] = True
-            elif rp.can_fetch('*', ref):
-                #print("Can fetch!")
-                in_host[ref] = True
-
-        elif ref.startswith('htt'): #either http or https; link to other site
-            if not ref.endswith('.html') and ref.split('/').__len__() > 3 and '.' in  ref.split('/')[ref.split('/').__len__()-1] : #it`s probably css or jpg or whateva; ends up with .../<random>.<sth>
-                continue #ignore it. it`s crap.
-
-            if ref.startswith(link):
-                in_host[ref] = True
-                continue #don`t want to be added into out links
-            out_host[ref]=True #maybe other data structure?
-            continue #everything is done
-
-        elif ref.endswith('.html'): #another type of links  #dont`t know if this part is working
-            in_host[ host_site_url + ref] = True
-    #for ref in re.findall('')
+#TODO remember about variables that are set as global.
 
 
 def check_robots(host):   #this part has to be reworked
 
+    global can_browse
+    global robots_broken
+    can_browse = True
+    robots_broken = True
+    global rp
     rp = urllib.robotparser.RobotFileParser()
     rp.set_url( host + 'robots.txt')  #tu wrzuca się hosta w link
 
     try:
         rp.read()
+
         can_browse = rp.can_fetch("*", host)
+
         return rp
     except Exception:       #catches exception as it can`t open and read non existing robots.txt, so everything is allowed
         #print( "rp.read() nie dziala : (" )
         can_browse = True #it will be easier and more secure to use it in next "if" so i just set it true. no robots means doors open : )
         robots_broken = True
+def look_for_links(page_in_string):
 
+    global out_host
+    global in_host
+    out_host = {}
+    in_host = {}
 
+    no_follow = False
+    #TODO make good meta-tag behavior
+
+    if re.findall('meta.*robots.*noindex' ,page_in_string):
+        print ("Nie indeksujemy strony!")
+        no_index = True
+        return False  #should end
+    if re.findall('meta.*robots.*nofollow' ,page_in_string):
+        print ("Nie idziemy stąd dalej!!")
+        no_follow = True
+        return True
+    if re.findall('meta.*robots.*none' ,page_in_string):
+        print ("Nie indeksujemy i nie idziemy dalej!!")
+        no_follow = True
+        return False
+    if not no_follow:
+
+        for ref in re.findall('(src|href)="(\S+)"', page_in_string):
+            ref = ref[1]
+
+            if ( not ref.endswith('.html') ) and ref.split('/').__len__() > 3 and '.' in  ref.split('/')[ref.split('/').__len__()-1] : #it`s probably css or jpg or whateva; ends up with .../<random>.<sth>
+                    continue #ignore it. it`s crap.
+
+            if ref.startswith('/'): #it`s link to some sub-site
+               ref = "{0}{1}".format(link , ref)
+               if robots_broken:
+                    #print("Can fetch! << robots_broken")
+                    in_host[ref] = True
+               elif rp.can_fetch('*', ref):
+                    #print("Can fetch!")
+                    in_host[ref] = True
+
+            elif ref.startswith('htt'): #either http or https; link to other site
+                if not ref.endswith('.html') and ref.split('/').__len__() > 3 and '.' in  ref.split('/')[ref.split('/').__len__()-1] : #it`s probably css or jpg or whateva; ends up with .../<random>.<sth>
+                    continue #ignore it. it`s crap.
+
+                if ref.startswith(link):
+                    in_host[ref] = True
+                    continue #don`t want to be added into out links
+                out_host[ref]=True #maybe other data structure?
+                continue #everything is done
+
+            elif ref.endswith('.html'): #another type of links  #dont`t know if this part is working
+                in_host[ base_url + '/'+ ref] = True
+        return True
 def set_correct_url(url):
     if not (url.startswith("http://") or url.startswith("https://")):
         return 'http://' + url
     else:
         return url
-### to being considered:
-### what to do with found links and where and how should they be stored. If everything together, or those to another host in some "higher" dict/set
-
-## GENERAL TODO add request/time limitation
-## givem limit is x/hour ;why not make it other?
-
-link = sys.argv[1]
-#link = 'https://www.facebook.com/'
-#link = 'http://edi.iem.pw.edu.pl/~maslowss/test.html'
-#link = 'http://edi.iem.pw.edu.pl/~maslowss/PolitBot/forbid.html'
-#link = 'isod.ee.pw.edu.pl'  #tu sa typy odnosnikow do sprawdzenia!!! a href='plik.html'
-
-
-
-base_url = set_correct_url(link)
-rp = check_robots(link)
-print ('Link z którego wchodzimy :',link)
-
-
-if  can_browse or robots_broken:
+def get_site_content():
+    if  can_browse or robots_broken:
     #print("Moze!")
-    req = urllib.request.Request(
-            base_url,
-            data = None,
-            headers = {
-                'User-Agent': 'PolitBot ( http://edi.iem.pw.edu.pl/0~maslowss/PolitBot/index.html )',  # gdzies tu sie psuje ; (; przecinki? albo cos z adresem strony, to ostatnio zmieniałem
-            }
-    )
-    resp  = urllib.request.urlopen(req)
-
-    ## TODO check if the host is introducing himself (name, version, operating sys)
-    ## TODO what if not ? :)
-
-
-    site_str = str( resp.read() )
-
-    if re.findall('meta.*robots.*noindex' ,site_str):
-        print ("Nie indeksujemy strony!")
-
-    if re.findall('meta.*robots.*nofollow' ,site_str):
-        print ("Nie idziemy stąd dalej!!")
-
-    if re.findall('meta.*robots.*none' ,site_str):
-        print ("Nie indeksujemy i nie idziemy dalej!!")
-
-  # <====== important!! result being initialised here!!!!!!
-
-    #links = re.findall('"(https?://.*?)"', site_str )  #probably will be erased, but don`t have time to look at it, lel
-    #print ("Linki ktore znaleziono : " )
-    #print( links )
-    #print("Linki posrednie: ")
-    #print (re.findall('(src|href)="(\S+)"', site_str))
-    #a = re.findall('(src|href)="(\S+)"', site_str)
-
-    #for x in a:
-    #    print(x)
-
-    look_for_links(site_str)
-
-    ## TODO look for forms at the page/application?
-    ## TODO check if easy/default passwords can give me acces : 3
-    ## TODO easy SQL injection, ahh think of it as late todo :D
-
-
+        req = urllib.request.Request(
+                base_url,
+                data = None,
+                headers = {
+                 'User-Agent': 'PolitBot ( http://edi.iem.pw.edu.pl/0~maslowss/PolitBot/index.html )',  # gdzies tu sie psuje ; (; przecinki? albo cos z adresem strony, to ostatnio zmieniałem
+                }
+        )
+        resp  = urllib.request.urlopen(req)
+        #def check_server_introduction() #TODO implement it
+        return str( resp.read() )
+    else:
+        #print ('Nie moze!')
+        return '' #empty string; it`s not bug :D
+def print_links():
     a = 0
     print('Inside : ')
     for l in in_host.keys():
         print (l)
         a+=1
     print('Ogolem linkow wewnatrz : ',a)
-
     a = 0
     print('Outside : ')
     for l in out_host.keys():
         print (l)
         a+=1
     print('Ogolem linkow zewnatrz : ',a)
-else:
-    print("Nie moze!")
+def crawl(start_link):
+    global base_url
+    base_url = set_correct_url(link)
+    rp = check_robots(link)
+    site_str = get_site_content()
+    if look_for_links(site_str): #True if can browse in them. If false then, cant
+        print_links()
 
-#print(">>>>" + link + "<<<<")
+    ## TODO look for forms at the page/application?
+    ## TODO check if easy/default passwords can give me acces : 3
+    ## TODO easy SQL injection, ahh think of it as late todo :D
+
+
+
+### to being considered:
+### what to do with found links and where and how should they be stored. If everything together, or those to another host in some "higher" dict/set
+
+## GENERAL TODO add request/time limitation
+## givem limit is x/hour ;why not make it other?
+
+
+
+link = sys.argv[1]
+link = 'https://www.facebook.com/'
+#link = 'http://edi.iem.pw.edu.pl/~maslowss/test.html'
+#link = 'http://edi.iem.pw.edu.pl/~maslowss/PolitBot/forbid.html'
+#link = 'isod.ee.pw.edu.pl'
+
+crawl(link)
+print ('Link z którego wchodzimy :',base_url)
+
+
 
 
